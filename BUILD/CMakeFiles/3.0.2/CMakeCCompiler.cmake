64cb8575d62bc174dfe525947567f3ae29b69dc4
Wassim_home <wmk1011@gmail.com>
2017-12-12 01:54:45 +0100

    nightly

--- BUILD/CMakeFiles/3.0.2/CMakeCCompiler.cmake
+++ BUILD/CMakeFiles/3.0.2/CMakeCCompiler.cmake
@@ -1,7 +1,7 @@
-set(CMAKE_C_COMPILER "/opt/intel/bin/icc")
+set(CMAKE_C_COMPILER "/usr/bin/cc")
 set(CMAKE_C_COMPILER_ARG1 "")
-set(CMAKE_C_COMPILER_ID "Intel")
-set(CMAKE_C_COMPILER_VERSION "17.0.1.20161005")
+set(CMAKE_C_COMPILER_ID "GNU")
+set(CMAKE_C_COMPILER_VERSION "4.9.2")
 set(CMAKE_C_PLATFORM_ID "Linux")
 set(CMAKE_C_SIMULATE_ID "")
 set(CMAKE_C_SIMULATE_VERSION "")
@@ -9,7 +9,7 @@ set(CMAKE_C_SIMULATE_VERSION "")
 set(CMAKE_AR "/usr/bin/ar")
 set(CMAKE_RANLIB "/usr/bin/ranlib")
 set(CMAKE_LINKER "/usr/bin/ld")
-set(CMAKE_COMPILER_IS_GNUCC )
+set(CMAKE_COMPILER_IS_GNUCC 1)
 set(CMAKE_C_COMPILER_LOADED 1)
 set(CMAKE_C_COMPILER_WORKS TRUE)
 set(CMAKE_C_ABI_COMPILED TRUE)
@@ -50,8 +50,8 @@ endif()
 
 
 
-set(CMAKE_C_IMPLICIT_LINK_LIBRARIES "imf;svml;irng;m;ipgo;decimal;cilkrts;stdc++;irc;svml;c;irc_s;dl;c")
-set(CMAKE_C_IMPLICIT_LINK_DIRECTORIES "/opt/intel/compilers_and_libraries_2017.1.132/linux/tbb/lib/intel64_lin/gcc4.7;/opt/intel/compilers_and_libraries_2017.1.132/linux/compiler/lib/intel64_lin;/opt/intel/compilers_and_libraries_2017.1.132/linux/mkl/lib/intel64_lin;/usr/lib/gcc/x86_64-linux-gnu/4.9;/usr/lib/x86_64-linux-gnu;/usr/lib;/lib/x86_64-linux-gnu;/lib64;/lib;/usr/lib/i386-linux-gnu")
+set(CMAKE_C_IMPLICIT_LINK_LIBRARIES "c")
+set(CMAKE_C_IMPLICIT_LINK_DIRECTORIES "/usr/lib/gcc/x86_64-linux-gnu/4.9;/usr/lib/x86_64-linux-gnu;/usr/lib;/lib/x86_64-linux-gnu;/lib")
 set(CMAKE_C_IMPLICIT_LINK_FRAMEWORK_DIRECTORIES "")
 
 
