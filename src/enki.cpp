0a7617916890fcdc5f9d94258b7a834bc8def3e7
Wassim Kassem <wk234832@dell455102>
2017-12-18 18:51:59 +0100

    moved stuff to enk - fix mainkhater

--- src/enki.cpp
+++ src/enki.cpp
@@ -137,3 +137,236 @@ void enki::wrap_atoms_box(std::vector<Atom>& atoms,
     atoms[i].coords = rAtom;
   }
 }
+
+int enki::create_perfect( const UnitCell& cell,
+                    const Eigen::Matrix<float,3,2>& Nx,
+                    std::vector<Atom>& atoms,
+                    Eigen::Matrix<float,3,4>& box){
+
+  UnitCell cell0(cell); // create a temporary unit cell
+  Crystal xtal(0, Nx, &cell0);
+  int Natoms = xtal.build(atoms);
+  enki::get_box_vectors(cell, Nx, box);
+  return Natoms;
+
+}
+
+int enki::create_edge_xz(const UnitCell& cell,
+                   const Eigen::Matrix<float,3,2>& Ntiles,
+                   std::vector<Atom>& atoms1,
+                   Eigen::Matrix<float,3,4>& box1,
+                   std::vector<Atom>& atoms2,
+                   Eigen::Matrix<float,3,4>& box2){
+
+  //
+  int Nx = Ntiles(0,1) - Ntiles(0,0);
+  int Ny = Ntiles(1,1) - Ntiles(1,0);
+  int Nz = Ntiles(2,1) - Ntiles(2,0);
+  float b = cell.ucv.block<3, 1>(0, 0).norm();
+
+  // find the mid-plane along the Z-direction
+  int midz = Ntiles(2,1) + Ntiles(2,0);
+  if (midz % 2 != 0){
+    midz++;
+  }
+  midz = (int)midz / 2;
+
+  // Create Lambda Mu unit cells
+  UnitCell ucLambda(cell);
+  UnitCell ucMu(cell);
+  ucLambda.ucv(0, 0) -= 0.5*b/Nx;
+  ucMu.ucv(0,0) += 0.5*b/Nx;
+
+  // Repeats
+  Eigen::Matrix<float,3,2> Nlambda = Ntiles;
+  Eigen::Matrix<float,3,2> Nmu = Ntiles;
+  Nmu(0,1)--;
+  Nmu(2,1) = midz;
+  Nlambda(2,0) = midz;
+
+  // Crystals
+  Crystal mu(0, Nmu, &ucMu);
+  Crystal lambda(1, Nlambda, &ucLambda);
+
+  // Atoms and boxes
+  int N1 = mu.build(atoms1);
+  int N2 = lambda.build(atoms2);
+  enki::get_box_vectors(ucMu, Nmu, box1);
+  enki::get_box_vectors(ucLambda, Nlambda, box2);
+
+  return N1+N2;
+
+}
+
+int enki::create_screw_xz( const UnitCell& cell,
+                     const Eigen::Matrix<float,3,2>& Nx,
+                     std::vector<Atom>& atoms1,
+                     Eigen::Matrix<float,3,4>& box1,
+                     std::vector<Atom>& atoms2,
+                     Eigen::Matrix<float,3,4>& box2){
+
+  float b = cell.ucv.block<3,1>(0,0).norm();
+  float dy = cell.ucv.block<3,1>(0,1).norm();
+  float Ny = Nx(1,1) - Nx(1,0);
+  float Nz = Nx(2,1) - Nx(2,0);
+
+  Eigen::Matrix3f ucShift;
+  ucShift << 0., -0.5*b/Ny, 0.,
+    0., 0., 0.,
+    0., 0., 0.;
+
+  UnitCell lambdaUC(cell);
+  lambdaUC.ucv += ucShift;
+  //std::cout << "ddd lambda_uc=\n" << lambdaUC.ucv << std::endl;
+  //ucShift(0,1) *= -1.;
+  UnitCell muUC(cell);
+  muUC.ucv -= ucShift;
+  //std::cout << "ddd mu_uc=\n" << muUC.ucv << std::endl;
+  //std::cout << "ddd uc=\n" << cell.ucv << std::endl;
+
+  Eigen::Matrix<float,3,2> Nlambda = Nx;
+  Eigen::Matrix<float,3,2> Nmu = Nx;
+  Nmu(2,1) = Nmu(2,0) + (int)Nz/2;
+  Nlambda(2,0) = Nmu(2,1);
+
+  //std::cout << "ddd Nmu=\n" << Nmu << std::endl;
+  //std::cout << "ddd Nlambda=\n" << Nlambda << std::endl;
+
+  Crystal lambda (0, Nlambda, &lambdaUC);
+  Crystal mu (1, Nmu, &muUC);
+
+  int N1 = mu.build(atoms1);
+  int N2 = lambda.build(atoms2);
+
+  enki::get_box_vectors(muUC, Nmu, box1);
+  enki::get_box_vectors(lambdaUC, Nlambda, box2);
+
+  box2(0,1) = box1(0,1);
+
+  return N1+N2;
+
+
+}
+
+/**
+   Create a SIA loop by adding atoms to the list of atoms.
+   @cell is the unit cell used to create the crystal
+   @tiles is the number of tilings of the unit cell
+   @loopMiller 3x2 matrix representing the smallest element of the loop. Normally these will be integers
+   @loopTiles a 2x2 matrix defnining how many tiles of the loop to create. These are in loop-space
+   @siaV a vector representing the intersitial atom direction in loop-space
+   @atoms the vector of append to
+ **/
+
+int enki::create_sia_loop ( const UnitCell& cell,
+                      const Eigen::Matrix<float,3,2>& tiles,
+                      const Eigen::Matrix<float,3,3>& loopMiller,
+                      const Eigen::Matrix<float,3,2>& loopTiles,
+                      const Eigen::Vector3f& sia,
+                      std::vector<Atom>& atoms,
+                      Eigen::Matrix<float,3,4>& box ){
+
+  // First we need to find the unit cell of the loop
+  UnitCell loopCell;
+  // loop contains the vectors (and base atoms) of the loop in real-space
+  enki::transform(cell, loopMiller, loopCell);
+  float eps = 0.001;
+
+  std::cout << "ddd new loopCell = " << std::endl;
+  loopCell.print_me();
+
+  for (int i = 0; i < loopCell.number_of_atoms(); ++i){
+    loopCell.basis.block<3,1>(0,i) = loopCell.basis.block<3,1>(0,i) + (eps * sia);
+  }
+
+  std::cout << "ddd new loopCell = " << std::endl;
+  loopCell.print_me();
+
+  // ensure that at least one of the tilings has a length of 1
+  /*
+  bool isLoop = false;
+  for (int i = 0; i < 3; ++i){
+    if ( loopTiles(i,1) - loopTiles(i,0) < 1.01 ) { isLoop = true;}
+  }
+  if (false == isLoop) {
+    std::cout << "ERROR: Cannot create loop. Must have one side length equal to 1" << std::cout;
+    return 0;
+  }
+  */
+  std::cout << "ddd loopTIles = " << std::endl << loopTiles << std::endl;
+  Crystal loop(-1, loopTiles, &loopCell);
+  int nLoopAtoms = loop.build(atoms);
+  enki::get_box_vectors (loopCell, loopTiles, box);
+
+  // Now loop over all atoms and find inside the loop
+  /*
+  int nLoopAtoms = 0;
+  int oldN = atoms.size();
+  Eigen::Matrix3f invLoop = loop.block<3,3>(0,0).inverse();
+  Eigen::Vector3f scAtom, rAtom;
+
+  for (int ia = 0; ia < oldN ; ++ia){
+    scAtom = invLoop * atoms[ia].coords; // calculate coorindates in crystal-space
+    scAtom = scAtom - loop.block<3,1>(0,3); // shift to loop origin
+    std::cout << "ddd test atom = " << scAtom.transpose() << std::endl;
+    for (int ix = 0; ix < 3; ++ix){ if (abs(scAtom(ix)) > 0){ continue; } }
+    std::cout << "ddd loop atom = " << scAtom.transpose() << std::endl;
+    scAtom = scAtom + loop.block<3,1>(0,3); // shift back to position
+    scAtom = scAtom + (eps * sia); // add a small shift SIA
+    rAtom = loop.block<3,3>(0,0) * scAtom; // calculate coordinates in real space
+    atoms.push_back(Atom(oldN + nLoopAtoms, scAtom, rAtom)); // add SIA
+    nLoopAtoms++;
+  }
+  */
+
+  return nLoopAtoms;
+
+}
+
+/**
+   Write an atom list to a LAMMPS data file
+   Only orthogonal box, 1 atom type, 1 header, and 1 orientation defined for the moment
+ **/
+int enki::write_lammps_data_file( std::FILE* fstream,
+                            std::vector<Atom>& atomList,
+                            Eigen::Matrix<float,3,4>& boxLims ){
+
+  // count number of valid atoms
+  int natoms = atomList.size();
+  for (int i = 0; i < atomList.size(); ++i){
+    if (atomList[i]._id == -1){ natoms--; }
+  }
+
+  // make sure the box is upper triangular
+  for (int i = 1; i < 3; ++i){
+    for (int j = i-1; j < i; ++j){
+      if (boxLims(i,j) > 1.e-4){
+        std::cout << "ERROR: Crystal is not well oriented, no atoms written" << std::endl;
+        return 0;
+      }
+    }
+  }
+
+  // write the header
+  fprintf ( fstream, "#ver .1, HCP-Zr [11-20], [0001], [-1100], metal units\n");
+  fprintf ( fstream, "    %i atoms\n", natoms);
+  fprintf ( fstream, "    1 atom types\n\n");
+  fprintf ( fstream, "%1.6f %1.6f xlo xhi\n", boxLims(0,3), boxLims(0,0)+boxLims(0,3));
+  fprintf ( fstream, "%1.6f %1.6f ylo yhi\n", boxLims(1,3), boxLims(1,1)+boxLims(1,3));
+  fprintf ( fstream, "%1.6f %1.6f zlo zhi\n", boxLims(2,3), boxLims(2,2)+boxLims(2,3));
+  fprintf ( fstream, "%1.6f %1.6f %1.6f xy xz yz\n\n", boxLims(0,1), boxLims(0,2), boxLims(1,2));
+  fprintf ( fstream, "Masses\n\n");
+  fprintf ( fstream, "    1 91.224\n\n");
+  fprintf ( fstream, "Atoms\n");
+
+  int iatom = 0;
+  for ( int i=0; i<atomList.size(); ++i){
+    if (atomList[i]._id == -1){ continue; }
+    iatom++;
+    fprintf ( fstream, "\n\t%i\t%i\t %1.6f %1.6f %1.6f", iatom, 1,
+              atomList[i].coords(0), atomList[i].coords(1), atomList[i].coords(2));
+  }
+
+  return iatom;
+
+}
--- src/enki.h
+++ src/enki.h
@@ -31,6 +31,60 @@ namespace enki{
   void wrap_atoms_box (std::vector<Atom>& atoms,
                        const Eigen::Matrix<float,3,4>& box);
 
+  /**
+     \brief Creates Atom::Atom using a UnitCell::UnitCell and a repition patters
+     \param cell The unit cell
+     \param Nx A 3-by-2 matrix indicating the position of the lowest and higest unit cell
+     \param atom A vector of Atom::Atom to add to
+     \param box A 3-by-4 matrix representing a box
+
+     The method produces atoms by creating tilings and then generating atoms with those tilings
+   **/
+  int create_perfect( const UnitCell& cell,
+                      const Eigen::Matrix<float,3,2>& Nx,
+                      std::vector<Atom>& atoms,
+                      Eigen::Matrix<float,3,4>& box);
+  /**
+     \brief Create a crystal with an edge dislocation along the <a2> direction using Bacon's method
+     \param cell The perfect UnitCell::UnitCell
+     \param Ntiles A 3-by-2 matrix representing the tilings to use for the dislocated crystal
+     \param atoms1 A vector to store the atoms in the lower half (mu) of the crystal
+     \param box1 A 3-by-4 matrix representing the box holding the lower atoms
+     \param atoms2 A vector to store tha atoms in the upper half (lambda) of the crystal
+     \param box2 A 3-by-4 matrix representing the box holding the top part of the atoms
+
+     The method creates a dislocated crystal by returning two halves having different lattice spacings
+     along the <a1> direction.
+
+     \attention The method should work for non-orthogonal unit cells, but has not been tested
+     \todo Test the method with non-orthogonal unit cell
+  **/
+  int create_edge_xz(const UnitCell& cell,
+                     const Eigen::Matrix<float,3,2>& Ntiles,
+                     std::vector<Atom>& atoms1,
+                     Eigen::Matrix<float,3,4>& box1,
+                     std::vector<Atom>& atoms2,
+                     Eigen::Matrix<float,3,4>& box2)
+
 };
+
+create_screw_xz( const UnitCell& cell,
+                 const Eigen::Matrix<float,3,2>& Nx,
+                 std::vector<Atom>& atoms1,
+                 Eigen::Matrix<float,3,4>& box1,
+                 std::vector<Atom>& atoms2,
+                 Eigen::Matrix<float,3,4>& box2);
+
+create_sia_loop ( const UnitCell& cell,
+                  const Eigen::Matrix<float,3,2>& tiles,
+                  const Eigen::Matrix<float,3,3>& loopMiller,
+                  const Eigen::Matrix<float,3,2>& loopTiles,
+                  const Eigen::Vector3f& sia,
+                  std::vector<Atom>& atoms,
+                  Eigen::Matrix<float,3,4>& box );
+
+write_lammps_data_file ( std::FILE* fstream,
+                         std::vector<Atom>& atomList,
+                         Eigen::Matrix<float,3,4>& boxLims );
 
 #endif
